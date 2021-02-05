3eae3f823d069dd575aa237f7b057fd9db1b9255
Jonas Lima <jonaslima015@gmail.com>
2021-02-05 18:43:56 -0300

    mostrando array numa tabela

--- src/components/Cadastro.vue
+++ src/components/Cadastro.vue
@@ -4,34 +4,38 @@
         <form action method v-on:submit.prevent="salvarDados">
             <label>Nome:</label>
             <input
-                v-model="user.nome"
+                v-model="nome"
                 type="text"
                 class="form-control"
                 placeholder="Ex: João"
+                required
             />
             <br>
             <label>Sobrenome:</label>
             <input
-                v-model="user.sobrenome"
+                v-model="sobrenome"
                 type="text"
                 class="form-control"
                 placeholder="Ex: Silva"
+                required
             />
             <br>
             <label>Email:</label>
             <input
-                v-model="user.email"
+                v-model="email"
                 type="text"
                 class="form-control"
                 placeholder="Ex: email@example.com"
+                required
             />
             <br>
             <label>Telefone:</label>
             <input
-                v-model="user.telefone"
+                v-model="telefone"
                 type="text"
                 class="form-control"
                 placeholder="Ex: (XX) XXXXX-XXXX"
+                required
             />
             <br>
             <input 
@@ -47,7 +51,7 @@
                 id="cpf"
                 class="form-control"
                 placeholder="Ex: XXX.XXX.XXX-XX"
-                v-model="user.cpf"
+                v-model="cpf"
                 v-if="!pj"
             />
             <label v-if="pj">CPNJ:</label>
@@ -55,43 +59,64 @@
                 type="text"
                 class="form-control"
                 placeholder="Ex:  XX.XXX.XXX/0001-XX"
-                v-model="user.cnpj"
+                v-model="cnpj"
                 v-if="pj"
             />
             <br>
-
             <button>
             <span>Salvar</span>
           </button>
         </form>
+        <Tabela :users="users" />
     </div>
 </template>
 
 <script>
+import Tabela from './Tabela.vue';
+
 export default {
   name: "Cadastro",
+  components: {
+    Tabela,
+  },
   data() {
     return{
       users: [],
+      nome: null,
+      sobrenome: null,
+      telefone: null,
+      email: null,
+      cnpj: null,
+      cpf: null,
       pj: false,
-      user: {
-        nome: null,
-        sobrenome: null,
-        email: null,
-        cnpj: null,
-        cpf: null,
-      }
+    }
+  },
+  computed: {
+    nomeCompleto: {
+      get: function (){
+        return this.nome + ' ' + this.sobrenome
+      },
     }
   },
   methods: {
     salvarDados(){
       this.users.push({
-        nome: this.user.nome + ' ' + this.user.sobrenome,
-        email: this.user.email,
-        telefone: this.user.telefone,
-        cnpj: this.user.cnpj,
-        cpf: this.user.cpf,
+        nome: this.nomeCompleto,
+        email: this.email,
+        telefone: this.telefone,
+        cnpj: this.cnpj,
+        cpf: this.cpf,
+        pj: this.pj,
       });
+
+      this.nome = '';
+      this.sobrenome = '';
+      this.email = '';
+      this.telefone = '';
+      this.cpf = '';
+      this.cnpj = '';
+      this.pj = false;
+
       console.log(this.users);
     }
   }
---
+++ src/components/Tabela.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <td>Nome completo</td>
+                    <td>Email</td>
+                    <td>Telefone</td>
+                    <td>Pessoa Jurídica?</td>
+                    <td>Ação</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in users" :key="user.id">
+                    <td>{{user.nome}}</td>
+                    <td>{{user.email}}</td>
+                    <td>{{user.telefone}}</td>
+                    <td>{{user.pj}}</td>
+                    <td>
+                        <button>Editar</button>
+                        <button>Apagar</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['users'],
+}
+</script>
+
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
